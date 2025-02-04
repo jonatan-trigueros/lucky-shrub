e86b884f2bc7aa9f269b4ad6f4c09815624332a1
Jonatan Trigueros <jonatantrigueros12@gmail.com>
2025-02-03 21:25:21 -0600

    code update

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const autoWidth = () => {
-    document.querySelectorAll(".styles__item-info, .services__item-info").forEach((article) => {
-      const title = article.querySelector(
-        ".styles__item-title, .services__item-title"
-      );
-      article.style.width = title.offsetWidth + "px";
-    });
+  const autoWidth = (title) => {
+    document
+      .querySelectorAll(".styles__item-info, .services__item-info")
+      .forEach((itemInfo) => {
+        title = itemInfo.querySelector(
+          ".styles__item-title, .services__item-title"
+        );
+        itemInfo.style.width = title.offsetWidth + "px";
+      });
   };
-  autoWidth();
-  window.addEventListener("resize", autoWidth);  
+  const openMobileNav = (burguer, mobileNav) => {
+    burguer = document.getElementById("burguer");
+    mobileNav = document.getElementById("mobile-navigation");
+
+    burguer.classList.toggle("active");
+    mobileNav.classList.toggle("open");
+
+    burguer.classList.contains("active")
+      ? burguer.setAttribute("aria-label", "Close navigation")
+      : burguer.setAttribute("aria-label", "Open navigation");
+
+    mobileNav.classList.contains("open")
+      ? mobileNav.setAttribute("aria-hidden", "false")
+      : mobileNav.setAttribute("aria-hidden", "true");
+  };
+  const heroInfoColorChage = () => {
+    setTimeout(() => {
+      document
+        .querySelectorAll(".hero-title, .hero__description")
+        .forEach((text) => {
+          text.style.opacity = 1;
+        });
+    }, 500);
+
+    setTimeout(() => {
+      document.getElementById("hero__projects-link").style.opacity = 1;
+    }, 7500);
+  };
+
+  heroInfoColorChage();
+  window.innerWidth > 1024 ? autoWidth() : null;
+  document.getElementById("burguer").addEventListener("click", openMobileNav);
 });
-
-
-const autoWidth = () => {
-  document.querySelectorAll(".styles__item-info, .services__item").forEach((article) => {
-    const title = article.querySelector(
-      ".styles__item-title, .services__item-title"
-    );
-    article.style.width = title.offsetWidth + "px";
-  });
-};
-
-
-
-
-
-
-
-
-
-
-
-
-document.getElementById("burguer").onclick = () => {
-  const burguer = document.getElementById("burguer");
-  const mobileNav = document.getElementById("mobile-navigation");
-  const firstLink = mobileNav.querySelector("a");
-
-  burguer.classList.toggle("active");
-  mobileNav.classList.toggle("open");
-
-  if (burguer.classList.contains("active")) {
-    burguer.setAttribute("aria-label", "Close navigation");
-  } else {
-    burguer.setAttribute("aria-label", "Open navigation");
-  }
-
-  if (mobileNav.classList.contains("open")) {
-    mobileNav.setAttribute("aria-hidden", "false");
-  } else {
-    mobileNav.setAttribute("aria-hidden", "true");
-  }
-
-  if (mobileNav.classList.contains("open")) {
-    firstLink.focus();
-  }
-};
